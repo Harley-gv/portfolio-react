773270eddd32ba24142858527f7a0220e1dabe36
Harley-gv <hegv1995@gmail.com>
2022-07-12 18:06:41 -0500

    fixes bug with github icon

--- src/assets/data/footerIcon.data.jsx
+++ src/assets/data/footerIcon.data.jsx
@@ -3,7 +3,7 @@ import {FaGithub,FaLinkedinIn} from 'react-icons/fa'
 export default [
     {
        "id": "1",
-       "url":"https://www.linkedin.com/in/harley-enrique-guerra-vergara-70b77b173/",
+       "url":"https://github.com/Harley-gv",
        "icon": <FaGithub/>
     },
     {
