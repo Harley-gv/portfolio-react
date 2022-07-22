4a3216efde629da73ecf6c6f37edeb122551d6b0
Harley-gv <hegv1995@gmail.com>
2022-07-21 19:48:55 -0500

    add project to portfolio data

--- src/assets/data/portfolio.data.jsx
+++ src/assets/data/portfolio.data.jsx
@@ -1,5 +1,6 @@
 import pokedex from '../img/portfolio1.jpg'
 import morty from '../img/portfolio2.jpg'
+import portfolio from '../img/portafolio.png'
 
 // queda pendiente agregar mas proyectos y adicional agregar imagenes reales de los proyectos ya que actual estan unas de prueba
 
@@ -20,6 +21,14 @@ export default [
         "github": "https://github.com/Harley-gv/rick_morty_react-app",
         "demo": "https://leafy-lollipop-016390.netlify.app",
         "description": "Proyecto web con React, usando Axios para consumir la API de Rick and Morty"
+    },
+    {
+        "id": "3",
+        "name": "Portafolio Web",
+        "img": portfolio,
+        "github": "https://github.com/Harley-gv/portfolio-react",
+        "demo": "#",
+        "description": "Proyecto realizado con React, simulando la data, consumiéndola desde arrays"
     },
       
      
